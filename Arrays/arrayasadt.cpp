4b90f022ff194c6d7afe650766b32f6a51328ed7
Saksham Sood <soodsakshamjan3@gmail.com>
2021-08-12 20:59:27 +0530

    adding a program for array as adt

--- Arrays/arrayasadt.cpp
+++ Arrays/arrayasadt.cpp
@@ -6,15 +6,59 @@ public:
     int *p;
     int size;
     int length;
-};
-void display(Array arr)
-{
-    cout << "Elements of the array are " << endl;
-    for (int i = 0; i < arr.length; i++)
+    void display()
     {
-        cout << arr.p[i] << " ";
+        cout << "Elements of the array are " << endl;
+        for (int i = 0; i < length; i++)
+        {
+            cout << p[i] << " ";
+        }
+        cout << endl;
     }
-}
+    void add(int x)
+    {
+        p[length] = x;
+        length++;
+        display();
+    }
+    void insert(int index, int element)
+    {
+        if (index >= 0 && index <= length)
+        {
+            for (int i = length; i > index; i--)
+            {
+                p[i] = p[i - 1];
+            }
+            p[index] = element;
+            length++;
+            display();
+        }
+        else
+        {
+            cout << "Not a valid index" << endl;
+            display();
+        }
+    }
+    void delete_element(int index)
+    {
+        int x = p[index];
+        if (index >= 0 && index < length)
+        {
+            for (size_t i = index; i < length - 1; i++)
+            {
+                p[i] = p[i + 1];
+            }
+            length--;
+            cout << "Element " << x << " was deleted" << endl;
+            display();
+        }
+        else
+        {
+            cout << "Not a valid index " << endl;
+            display();
+        }
+    }
+};
 int main()
 {
     int size, num;
@@ -31,6 +75,18 @@ int main()
         cin >> arr.p[i];
     }
     arr.length = num;
-    display(arr);
+    arr.display();
+    cout << "Enter an element to add or append to the array ";
+    int x;
+    cin >> x;
+    arr.add(x);
+    int ele, index;
+    cout << "Enter an element to insert and the index ";
+    cin >> ele >> index;
+    arr.insert(index, ele);
+    cout << "Enter an element to delete ";
+    cin >> ele;
+    arr.delete_element(ele);
+    delete[] arr.p;
     return 0;
 }
