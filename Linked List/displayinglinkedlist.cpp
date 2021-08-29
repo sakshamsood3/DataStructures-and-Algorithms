1662ebba9752c2eec19741c89cab0e66e36fe690
Saksham Sood <soodsakshamjan3@gmail.com>
2021-08-29 21:52:47 +0530

    program for displaying a linkes list

---
+++ Linked List/displayinglinkedlist.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+void create(int arr[], int n)
+{
+    struct Node *t;
+    struct Node *last;
+    first = new Node;
+    first->data = arr[0];
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = arr[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+}
+int main()
+{
+    int A[] = {3, 5, 7, 10, 15, 20};
+    create(A, 6);
+    display(first);
+    return 0;
+}
