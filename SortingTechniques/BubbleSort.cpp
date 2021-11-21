5120d42799e010b7d0e661ad06fb370fbb583dc3
Saksham Sood <soodsakshamjan3@gmail.com>
2021-11-21 17:50:47 +0530

    sorting techniques

---
+++ SortingTechniques/BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+void BubbleSort(int arr[], int n)
+{
+    int flag;
+    for (int i = 0; i < n - 1; i++)
+    {
+        flag = 0;
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                flag = 1;
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+}
+int main()
+{
+    int arr[] = {2, 42, 1, 32, 55};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    BubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
